5b37d24f3428cbd283681daac3299e371c25adb3
Josh Menden <josh.menden@hey.com>
2024-11-30 22:24:18 -0700

    Transpose in parse method

--- 2024/day-1/a.rb
+++ 2024/day-1/a.rb
@@ -6,17 +6,17 @@ require_relative '../aoc_solution'
 
 class Day < AOCSolution
   def parse_data
-    @data = @data.split("\n").map { |line| line.split(/\s+/).map(&:to_i) }
+    @data = @data.split("\n").map { |line| line.split(/\s+/).map(&:to_i) }.transpose
   end
 
   # 0.00069s
   def pt1
-    @data.transpose.map(&:sort).transpose.map { |a, b| (a - b).abs }.sum
+    @data.map(&:sort).transpose.map { |a, b| (a - b).abs }.sum
   end
 
   # 0.00013s
   def pt2
-    left, right = @data.transpose
+    left, right = @data
     tally = right.tally
     left.reduce(0) { |acc, num| acc + ((tally[num] || 0) * num) }
   end
