e81c78ce560226ec32c53bebd99d1aaa0843c191
Josh Menden <josh.menden@hey.com>
2024-11-30 22:15:20 -0700

    pt 1

--- 2024/day-1/a.rb
+++ 2024/day-1/a.rb
@@ -1,21 +1,20 @@
 #!/usr/bin/env ruby
 
-require "byebug"
-require "active_support/all"
-require_relative "../aoc_solution"
+require 'byebug'
+require 'active_support/all'
+require_relative '../aoc_solution'
 
 class Day < AOCSolution
   def parse_data
-    # access @data to parse input
+    @data = @data.split("\n").map { |line| line.split(/\s+/).map(&:to_i) }
   end
 
   def pt1
+    @data.transpose.map(&:sort).transpose.map { |a, b| (a - b).abs }.sum
   end
 
-  def pt2
-  end
+  def pt2; end
 end
 
-filename = "sample.txt"
-# filename = "input.txt"
+filename = 'input.txt'
 Day.new(filename).solve!
