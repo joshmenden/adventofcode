9d5cd86522f5fc7260f270f6961d6b0974e99500
Josh Menden <josh.menden@hey.com>
2024-12-01 22:13:46 -0700

    pt 1

--- 2024/day-2/a.rb
+++ 2024/day-2/a.rb
@@ -4,9 +4,14 @@ require_relative "../aoc_solution"
 
 class Day < AOCSolution
   def parse_data
+    @data = @data.split("\n").map { |line| line.split(/\s+/).map(&:to_i) }
   end
 
   def pt1
+    @data.count do |report|
+      (report == report.sort || report == report.sort.reverse) &&
+        report.each_cons(2).all? { |a, b| (a - b).abs >= 1 && (a - b).abs <= 3 }
+    end
   end
 
   def pt2
