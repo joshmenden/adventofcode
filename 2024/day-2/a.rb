6b072cb3f5c22ffd304e001e21e0019b2b6a15be
Josh Menden <josh.menden@hey.com>
2024-12-01 22:03:27 -0700

    Day 2 boilerplate

---
+++ 2024/day-2/a.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative "../aoc_solution"
+
+class Day < AOCSolution
+  def parse_data
+  end
+
+  def pt1
+  end
+
+  def pt2
+  end
+end
+
+filename = "input.txt"
+Day.new(filename).solve!
